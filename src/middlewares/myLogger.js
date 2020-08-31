a69eac161c5c88662577e1ca98714062816e4916
ParkSeongju <parksj914@naver.com>
2020-08-31 17:31:42 +0900

    미들웨어 프로토타입-수정

--- src/middlewares/myLogger.js
+++ src/middlewares/myLogger.js
@@ -1,6 +1,10 @@
 const myLogger = store => next => action => {
     console.log(action);
     const result = next(action); //다음 미들웨어에게 액션 전달
+
+    //업데이트 이후의 상태를 조회합니다.
+    console.log('\t', store.getState());
+
     return result; //반환 값: dispatch(action)결과물 기본: undefined
 };
 
