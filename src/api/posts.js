6f8f6dcc13d6e65336725f0c3e31704238cd4674
ParkSeongju <parksj914@naver.com>
2020-09-02 15:21:03 +0900

    proxy 설정

--- src/api/posts.js
+++ src/api/posts.js
@@ -3,16 +3,16 @@ import axios from 'axios';
 // 포스트 목록을 가져오는 비동기 함수
 export const getPosts = async () => {
     const response = await axios.get(
-        'http://localhost:4001/posts'
-    )
+        '/posts'
+    );
     return response.data;
 };
 
 // ID로 포스트를 조회하는 비동기 함수
 export const getPostById = async id => {
     const response = await axios.get(
-        `http://localhost:4001/posts/${id}`
-    )
+        `/posts/${id}`
+    );
     return response.data;
 };
 
